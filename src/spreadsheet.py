cf631969a60a2ff181c43cd997dd7a0778d6c272
Eric Wood <eric@ericwood.org>
2026-02-01 19:19:30 -0700

    Use correct arguments for merging symbol properties

--- src/spreadsheet.py
+++ src/spreadsheet.py
@@ -126,7 +126,7 @@ class Spreadsheet:
                 # only one symbol uses the template, so just clone/override it to avoid bloat
                 symbol = symbols[0]
                 new_symbol = deepcopy(template)
-                new_symbol.merge_properties(symbol)
+                new_symbol.merge_properties(symbol.properties)
                 library_symbols.append(symbol)
 
         # kicad is picky about symbol order; if you extend a symbol, that symbol needs to be first in the file
